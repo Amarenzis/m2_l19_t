e87c113c73df349d6faae0ba536375f5a631b25a
Дарья <sochidar@gmail.com>
2022-01-15 21:46:13 +0300

    Добавьте файлы проекта.

---
+++ Task1_CircleMVVM/Models/Geometry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_CircleMVVM.Models
+{
+    static public class Geometry
+    {
+        static public double LengthOfCircle(double r)
+        {
+            double l = 2 * Math.PI * r;
+            return l;
+        }
+    }
+}
---
+++ Task1_CircleMVVM/ViewModels/MainWindowVM.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Task1_CircleMVVM.Models;
+
+namespace Task1_CircleMVVM.ViewModels
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                if (value>0)
+                    radius = value;                
+                else                
+                    radius = 0;                
+                OnPropertyChanged();
+            }
+        }
+        private double circleLength;
+        public double CircleLength
+        {
+            get => circleLength;
+            set
+            {
+                if (value > 0)
+                    circleLength = value;
+                else
+                    circleLength = 0;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CircleLengthCommand { get; }
+        private void OnCircleLengthCommandExecute(object p)
+        {
+            CircleLength = Math.Round(Geometry.LengthOfCircle(Radius),2);
+        }
+        private bool CanCircleLengthCommandExecuted(object p)
+        {
+            if (Radius >= 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowVM()
+        {
+            CircleLengthCommand = new RelayCommand(OnCircleLengthCommandExecute, CanCircleLengthCommandExecuted);
+        }
+
+    }
+}
